d35a93c68fce2bf9caac87481c874e23fc1cfdb2
Martin Schmidt <martin@scoding.dk>
2022-04-01 13:23:16 +0200

    updated applications

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,17 @@
 
 # This script will boostrap the cluster by installing argocd with the help of helm-overdrive.
 # I am to lazy and forgetfull to remember the command.
-./helm-overdrive template -c helm-overdrive.yaml --application_folder services/argocd --helm_repo https://argoproj.github.io/argo-helm --app_name argocd --chart_name argo-cd --chart_version 4.4.0 | kubectl apply -n argocd -f -
+NAMESPACE=argocd
+
+export HO_APPLICATION_FOLDER=services/argocd
+export HO_HELM_REPO=https://argoproj.github.io/argo-helm
+export HO_CHART_NAME=argo-cd
+export HO_CHART_VERSION=4.4.0
+
+helm-overdrive template \
+    -c helm-overdrive.yaml \
+    --app_namespace $NAMESPACE \
+    --helm_repo https://argoproj.github.io/argo-helm \
+    --app_name argocd \
+    > a
+cat a | kubectl apply -n $NAMESPACE -f -
