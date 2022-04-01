42f1be9ef2dbd6e65463a3fd56cdc8a27ff051c4
Martin Schmidt <martin@scoding.dk>
2022-04-01 13:31:43 +0200

    fixed wrong namespace in argocd app

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,7 @@
 
 # This script will boostrap the cluster by installing argocd with the help of helm-overdrive.
 # I am to lazy and forgetfull to remember the command.
-NAMESPACE=argocd
-
+export HELM_NAMESPACE=argocd
 export HO_APPLICATION_FOLDER=services/argocd
 export HO_HELM_REPO=https://argoproj.github.io/argo-helm
 export HO_CHART_NAME=argo-cd
@@ -11,8 +10,6 @@ export HO_CHART_VERSION=4.4.0
 
 helm-overdrive template \
     -c helm-overdrive.yaml \
-    --app_namespace $NAMESPACE \
     --helm_repo https://argoproj.github.io/argo-helm \
     --app_name argocd \
-    > a
-cat a | kubectl apply -n $NAMESPACE -f -
+    | kubectl apply -n $HELM_NAMESPACE -f -
